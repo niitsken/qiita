5572ffc9c837b7c7c23c46a25c46095e219e6f64
新妻健介 <niits@niits.local>
2017-07-07 16:48:14 +0900

    Initial Commit

---
+++ Qiita/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qiita
+//
+//  Created by 新妻健介 on 2017/07/07.
+//  Copyright © 2017年 新妻健介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
